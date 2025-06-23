3f1f7eb6b3598eee5c78235a395a46380a9b8d98
txy <yangtianxiang0313@gmail.com>
2025-06-23 13:35:23 +0800

    Initial Commit

---
+++ legado/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  legado
+//
+//  Created by 杨天翔 on 23/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ legado/legadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  legadoApp.swift
+//  legado
+//
+//  Created by 杨天翔 on 23/6/25.
+//
+
+import SwiftUI
+
+@main
+struct legadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
